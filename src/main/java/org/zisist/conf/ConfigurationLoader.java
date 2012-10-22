458eb9f37f591b04a110936315069a42b4cc16f9
zztach <zis.tax@gmail.com>
2012-10-22 10:54:22 +0300

    Update src/main/java/org/zisist/conf/ConfigurationLoader.java

--- src/main/java/org/zisist/conf/ConfigurationLoader.java
+++ src/main/java/org/zisist/conf/ConfigurationLoader.java
@@ -5,6 +5,8 @@ import org.zisist.model.xml.LeaderboardConfiguration;
 import java.util.List;
 
 /**
+ * Responsible for loading the cofigurations needed to execute the algorithm<br>
+ * that handled the leaderbords<br>
  * Created by zis.tax@gmail.com on 07/10/2012 at 11:41 PM
  */
 public interface ConfigurationLoader {
