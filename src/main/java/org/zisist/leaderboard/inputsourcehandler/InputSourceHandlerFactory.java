00acb8069414fb0e75e0581eaf4a152812686958
zztach <zis.tax@gmail.com>
2012-10-22 10:27:51 +0300

    Update src/main/java/org/zisist/leaderboard/inputsourcehandler/InputSourceHandlerFactory.java

--- src/main/java/org/zisist/leaderboard/inputsourcehandler/InputSourceHandlerFactory.java
+++ src/main/java/org/zisist/leaderboard/inputsourcehandler/InputSourceHandlerFactory.java
@@ -1,6 +1,9 @@
 package org.zisist.leaderboard.inputsourcehandler;
 
 /**
+ * Responsible for retrieving the appropriate InputSourceHandler implementation <br>
+ * based on the provided <i>uri</i><br>
+ * 
  * Created by zis.tax@gmail.com on 12/10/2012 at 1:10 AM
  */
 public interface InputSourceHandlerFactory {
